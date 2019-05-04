2196ac4152a2b5626634def5cd148fae93f64939
evillt <ijoec123@gmail.com>
2019-05-04 11:11:13 +0800

    chore!: tweak based on loading-screen v1.1.0

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ exports.apply = api => {
           logo:
             'https://camo.githubusercontent.com/5ae09d1630be8e50dd69a50d9d45b326a0cb41ab/68747470733a2f2f692e6c6f6c692e6e65742f323031382f30392f31322f356239386537373335326339642e706e67',
           handler,
-          serverCallback() {
-            // Real • open browser
+          port,
+          // Real • open browser
+          callback() {
             if (args.has('o') || args.has('open')) {
               require('@poi/dev-utils/openBrowser')(
                 `http://${prettyHost}:${port}`
